f06572ecd17e3c915939e76de496ecfbe39fdf9a
eans25 <eangly.eng@gmail.com>
2018-07-31 14:24:28 +0200

    modif controller create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,10 @@ class UsersController < ApplicationController
   end
 
   def create
+  	@user = User.new
+        @user.username = params[:username]
+        @user.email = params[:email]
+        @user.bio = params[:bio]
+        @user.save
   end
 end
